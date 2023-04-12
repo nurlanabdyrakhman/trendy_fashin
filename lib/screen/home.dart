ad1bdb9ba238b68c8db424ad549b5026a9b9eeb6
Nurlan <abdyraknurlan@gmail.com>
2023-04-12 21:43:59 +0300

    12.04

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:trendy_fashion/model/categories_model.dart';
 import 'package:trendy_fashion/utils/constants.dart';
+import 'package:animate_do/animate_do.dart';
+
+import '../data/app_data.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,22 +25,78 @@ class _HomeState extends State<Home> {
           height: size.height,
           child: SingleChildScrollView(
             child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RichText(
-                  text: TextSpan(
-                      text: 'Find your',
-                      style: TextStyle(color: Colors.red, fontSize: 36),
+                FadeInUp(
+                  delay: Duration(milliseconds: 300),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        TextSpan(
-                          text: 'Style',
-                          style: theme.bodySmall?.copyWith(
-                            color: primaryColor,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
+                        RichText(
+                          text: TextSpan(
+                            text: 'Find your',
+                            style: TextStyle(color: Colors.red, fontSize: 36),
+                            children: [
+                              TextSpan(
+                                text: 'Style',
+                                style: theme.bodySmall?.copyWith(
+                                  color: primaryColor,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ]),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Be more beautiful with our  ',
+                            style: TextStyle(color: Colors.red, fontSize: 16),
+                            children: [
+                              TextSpan(
+                                text: 'suggestions:)',
+                                style: theme.bodySmall?.copyWith(
+                                  color: primaryColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 40,),
+                  width: size.width,
+                  height: size.height* 0.14,
+                  color: Colors.blue,
+                  child: ListView.builder(
+                   
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                   itemCount: categories.length,
+                    itemBuilder: (context,index){
+                      CategoriesModel current = categories[index];
+                   return Column(
+                    children: [
+                 CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage(current.imageUrl),
+                 
+                 ),
+                  SizedBox(height: size.height*0.008,),
+                  Text(
+                    current.title,
+                    style: theme.bodyMedium,
+                  )
+                    ],
+                   );
+                    },),
                 ),
               ],
             ),
